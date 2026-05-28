db28cdbeb1848ec99ed86dd477a3f31453c04317
SuperSH85 <shervin085@gmail.com>
2026-05-28 21:09:45 +0330

    feat: add Student class with menu and course methods

--- src/model/user/Student.java
+++ src/model/user/Student.java
@@ -1,4 +1,5 @@
 package model.user;
+import database.UniversitySystem;
 import model.course.Course;
 
 import java.util.*;
@@ -10,7 +11,37 @@ public class Student extends User {
     }
     @Override
     protected void showMenu() {
-
+        System.out.println("===== STUDENT MENU =====");
+        System.out.println("1. View All Courses");
+        System.out.println("2. Enroll Course");
+        System.out.println("3. Drop Course");
+        System.out.println("4. My Courses");
+        System.out.println("0. Logout");
+        System.out.println("========================");
     }
-    //اضافه کردن درس به لیست دانشجو
+
+    public void viewAllCourses(){
+        System.out.println("===== ALL COURSES =====");
+        for (Course course : UniversitySystem.getCourses()){
+            System.out.println(course);
+        }
+        System.out.println("======================");
+    }
+
+    public void enrollCourse(Course course){
+        this.courses.add(course);
+    }
+
+    public void dropCourse(Course course){
+        this.courses.remove(course);
+    }
+
+    public void getMyCourse(){
+        int temp = 1;
+        System.out.println("===== MY COURSES =====");
+        for (Course course : this.courses){
+            System.out.println((temp++) + ". " + course);
+        }
+        System.out.println("======================");
+    }
 }
