e4c3e1c1def9aaef7c10902f198172460b927447
SuperSH85 <shervin085@gmail.com>
2026-05-28 22:24:11 +0330

    feat: add welcome part to Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        var scn = new Scanner(System.in);
+        boolean flag = true;
+        while (flag){
+            System.out.println("╔═════════════════════════════════════════╗");
+            System.out.println("║       UNIVERSITY ENROLLMENT SYSTEM      ║");
+            System.out.println("║          Welcome! Please Login          ║");
+            System.out.println("╚═════════════════════════════════════════╝");
+            System.out.println();
+
+            System.out.println("Enter your Name: ");
+            String name = scn.nextLine();
+            System.out.println("Enter your Password: ");
+            String password = scn.nextLine();
+        }
+    }
 }
