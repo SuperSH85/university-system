74263d8a7688fac338d1df63c0afea9703af3a3e
SuperSH85 <shervin085@gmail.com>
2026-05-28 10:06:44 +0330

    add basic Course class with fields

--- src/model/course/Course.java
+++ src/model/course/Course.java
@@ -1,5 +1,32 @@
 package model.course;
+import model.Searchable;
+import model.user.Professor;
+import model.user.Student;
+import java.util.*;
 
-public class Course {
+public class Course implements Searchable {
 
+    private int courseID;
+    private String title;
+    private int credits;
+    private int capacity;
+    private CourseTime schedule;
+    private ArrayList<Student> students;
+    private Professor professor;
+    static int idMaker = 0;
+
+    public Course(String title ,
+                  int credits ,
+                  int capacity ,
+                  CourseTime schedule){
+        this.courseID = ++idMaker;
+        this.capacity = capacity;
+        this.credits = credits;
+        this.schedule = schedule;
+    }
+
+    @Override
+    public boolean matches(String keyword) {
+        return false;
+    }
 }
