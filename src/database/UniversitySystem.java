28d6dd50d1659afd4650f8a70f50ca60d2cd5cd2
SuperSH85 <shervin085@gmail.com>
2026-05-28 11:02:29 +0330

    feat: add in-memory database

--- src/database/UniversitySystem.java
+++ src/database/UniversitySystem.java
@@ -1,5 +1,27 @@
 package database;
 
+import model.course.Course;
+import model.user.User;
+import java.util.*;
+
 public class UniversitySystem {
+    private List<User> users = new ArrayList<>();;
+    private List<Course> courses = new ArrayList<>();;
+
+    //CR-User
+    protected void create(User user){
+        this.users.add(user);
+    }
+    protected void remove(User user){
+        this.users.remove(user);
+    }
+
+    //CR-Course
+    protected void create(Course course){
+        this.courses.add(course);
+    }
+    protected void remove(Course course){
+        this.courses.remove(course);
+    }
 
 }
